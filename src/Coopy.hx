c3e8e624bea3e2166612131b1a8ccc72dad3714f
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2013-01-25 22:45:50 +0100

    test whether external js objects can be swapped in, practically, without breaking c++ target

--- src/Coopy.hx
+++ src/Coopy.hx
@@ -6,6 +6,18 @@ class Coopy {
         x.set(1,"hi");
         trace("Hello world");
         trace("1 -> " + x.get(1));
+
+        var y : Hash<String> = new Hash<String>();
+        y.set("hello","world");
+        trace("hash " + y);
+        trace("hello - " + y.get("hello"));
+
+        var s : Store = new Store();
+        trace(s.frog == null);
+        s.frog = "space";
+        trace(s.frog);
+        trace(s);
+
         return 0;
     }
 }
