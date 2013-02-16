5f67ae338116f9dff44a0bd61438eabb6e0a6e89
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2013-02-16 22:47:45 +0100

    add missed file

---
+++ src/CrossIndexItem.hx
@@ -0,0 +1,13 @@
+// -*- mode:java; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+class CrossIndexItem {
+    public var act : Int;
+    public var bct : Int;
+    public var as : IntHash<Int>;
+    public var bs : IntHash<Int>;
+
+    public function new() : Void {
+        act = 0;
+        bct = 0;
+    }
+}
