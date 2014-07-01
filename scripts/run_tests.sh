6c1d22fd1706522ffeef62de390b62abce4c85e2
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2014-07-01 02:31:35 +0200

    nodejs -> node for travis

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -5,6 +5,6 @@ cd test || exit 1
 for f in `ls *$1*.js`; do
     echo "=============================================================================="
     echo "== $f"
-    NODE_PATH=$BASE/lib:$BASE/scripts nodejs --prof ./$f || exit 1
+    NODE_PATH=$BASE/lib:$BASE/scripts node ./$f || exit 1
 done
 echo "=============================================================================="
