0a17e041a471111d6aeec2a9a58245dbae41a340
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2014-10-15 16:03:26 +0200

    1.1.14.1

--- coopy/Coopy.hx
+++ coopy/Coopy.hx
@@ -6,7 +6,7 @@ package coopy;
 
 @:expose
 class Coopy {
-    static public var VERSION = "1.1.14";
+    static public var VERSION = "1.1.14.1";
 
     private var format_preference : String;
     private var delim_preference : String;
