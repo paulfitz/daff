9cc4eb24b64d6065443802a7442d01f9a1a7941a
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2014-11-12 05:56:55 +0100

    add python sqlite test

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,9 +2,14 @@
 
 BASE="$PWD"
 cd test || exit 1
-for f in `ls *$1*.js`; do
+for f in `ls -1 *.js | grep "$1"`; do
     echo "=============================================================================="
     echo "== $f"
     NODE_PATH=$BASE/lib:$BASE/scripts node ./$f || exit 1
+done
+for f in `ls -1 *.py | grep "$1"`; do
+    echo "=============================================================================="
+    echo "== $f"
+    PYTHONPATH=$PYTHONPATH:$BASE/python_bin python3 ./$f || exit 1
 done
 echo "=============================================================================="
