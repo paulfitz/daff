b10c789a6aae83c23322cf7cf96333b76773b988
Paul Fitzpatrick <paul.michael.fitzpatrick@gmail.com>
2015-10-09 09:50:38 -0400

    add reference diffs for sqlite

--- test/integration_sqlite.sh
+++ test/integration_sqlite.sh
@@ -5,6 +5,7 @@ set -e
 source scripts/language_environment.sh "$@"
 
 which sqlite3
+which diff
 
 {
     cat<<EOF
@@ -35,7 +36,14 @@ INSERT INTO t2 VALUES(4,101,"news");
 EOF
 } | sqlite3 v3.sqlite
 
-$DAFF_SCRIPT v1.sqlite v2.sqlite | tee log.txt
+$DAFF_SCRIPT v1.sqlite v2.sqlite | tee basic.diff
 $DAFF_SCRIPT v1.sqlite v2.sqlite
-$DAFF_SCRIPT v1.sqlite v3.sqlite
+$DAFF_SCRIPT v1.sqlite v3.sqlite | tee remove_table.diff
+$DAFF_SCRIPT v3.sqlite v1.sqlite | tee add_table.diff
 
+for diff in basic.diff add_table.diff remove_table.diff; do
+    diff -w ${diff} $org/test/sqlite/${diff} || {
+        echo "${diff} not created correctly."
+        exit 1
+    }
+done
