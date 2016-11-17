5597458410004885127ae4d27c05b6a4189b8eda
dplusic <dplusic@gmail.com>
2016-11-17 16:25:43 +0900

    Implement reading xlsx

--- coopy/XlsxJs.hx
+++ coopy/XlsxJs.hx
@@ -6,6 +6,7 @@ package coopy;
 
 class XlsxJs implements XlsxImpl {
     public function new() {
+        untyped __js__("if (!require('fibers').current) throw('run inside Fiber plz')");
     }
 
     public function create() : Workbook {
@@ -13,17 +14,30 @@ class XlsxJs implements XlsxImpl {
     }
 
     public function read(bytes: haxe.io.Bytes) : Workbook {
-        return null;
+        var workbook = new exceljs.Workbook();
+        var stream = new ReadableStreamBuffer();
+        stream.put(bytes);
+        stream.stop();
+        untyped __js__("require('fibers/future').fromPromise(workbook.xlsx.read(stream)).wait()");
+        return new XlsxJsWorkbook(workbook);
     }
 }
 
 class XlsxJsWorkbook implements Workbook {
+    private var workbook : exceljs.Workbook;
+
+    public function new(workbook: exceljs.Workbook) {
+        this.workbook = workbook;
+    }
+
     public function addWorksheet(name: String) : Worksheet {
         return null;
     }
 
     public function getWorksheet(index: Int) : Worksheet {
-        return null;
+        var adjustedIndex = index + 1;
+        var worksheet = workbook.getWorksheet(adjustedIndex);
+        return new XlsxJsWorksheet(worksheet);
     }
 
     public function getBytes() : haxe.io.Bytes {
@@ -32,10 +46,28 @@ class XlsxJsWorkbook implements Workbook {
 }
 
 class XlsxJsWorksheet implements Worksheet {
+    private var worksheet : exceljs.Sheet;
+
+    public function new(worksheet: exceljs.Sheet) {
+        this.worksheet = worksheet;
+    }
+
     public function getData() : Dynamic {
-        return null;
+        var rows = [];
+        worksheet.eachRow({ includeEmpty: true }, function (row, rowNumber) {
+            rows[rowNumber - 1] = row.values.slice(1);
+        });
+        return rows;
     }
 
     public function setCellValue(x: Int, y: Int, value: Dynamic) : Void {
     }
+}
+
+@:jsRequire("stream-buffers", "ReadableStreamBuffer")
+extern class ReadableStreamBuffer {
+    public function new();
+
+    public function put(bytes: haxe.io.Bytes) : Void;
+    public function stop() : Void;
 }
