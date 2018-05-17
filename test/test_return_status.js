a0d99afa9888dd233ba8432ac06b5af8f0480c9f
Paul Fitzpatrick <paul.michael.fitzpatrick@gmail.com>
2018-05-17 11:37:26 -0400

    add test for daff return status

---
+++ test/test_return_status.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var daff = require('daff');
+var tmp = require('tmp');
+
+tmp.tmpName({ template: '/tmp/tmp-XXXXXX.csv' },function(err, csv) {
+  if (err) throw err;
+  assert(0==daff.cmd(["diff","data/bridges.csv","data/broken_bridges.csv","--output",csv]));
+  assert(0==daff.cmd(["diff","data/bridges.csv","data/bridges.csv","--output",csv]));
+  assert(1==daff.cmd(["diff","--fail-if-diff","data/bridges.csv","data/broken_bridges.csv","--output",csv]));
+  assert(0==daff.cmd(["diff","--fail-if-diff","data/bridges.csv","data/bridges.csv","--output",csv]));
+  assert(2==daff.cmd(["diff","--fail-if-diff","data/bridges.csv","data/nothing.csv","--output",csv]));
+});
