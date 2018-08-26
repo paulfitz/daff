96a10f64310d182a97bd275192d696cc8cd3f7bc
Paul Fitzpatrick <paul.michael.fitzpatrick@gmail.com>
2018-08-25 22:26:05 -0400

    1.3.31

--- coopy/Coopy.hx
+++ coopy/Coopy.hx
@@ -17,7 +17,7 @@ class Coopy {
      * Library version.
      *
      */
-    static public var VERSION = "1.3.30";
+    static public var VERSION = "1.3.31";
 
     private var format_preference : String;
     private var delim_preference : String;
