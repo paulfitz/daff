f55b9b5af10a611334eaf6611980000d65b3e069
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2023-03-13 13:52:52 -0400

    1.3.48

--- coopy/Coopy.hx
+++ coopy/Coopy.hx
@@ -17,7 +17,7 @@ class Coopy {
      * Library version.
      *
      */
-    static public var VERSION = "1.3.47";
+    static public var VERSION = "1.3.48";
 
     private var format_preference : String;
     private var delim_preference : String;
